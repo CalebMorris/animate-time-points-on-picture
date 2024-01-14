a640664d1cac9517db073de26babe16cfcfa3859
Caleb Morris <caleb.morris.g@gmail.com>
2024-01-13 19:08:08 -0700

    Render the points!

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 const blobURLSupport = ((ref2 = window.URL) != null ? ref2.createObjectURL : void 0) != null;
 
+var baseImage
+
 function importImageToCanvas(canvas, ctx, e) {
     var reader = new FileReader();
     reader.onload = function (event) {
-        var img = new Image();
-        img.onload = function () {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
+        baseImage = new Image();
+        baseImage.onload = function () {
+            canvas.width = baseImage.width;
+            canvas.height = baseImage.height;
+            ctx.drawImage(baseImage, 0, 0);
         }
-        img.src = event.target.result;
+        baseImage.src = event.target.result;
         document.getElementById('canvas-container').style.display = null
         enableInputPoints();
     }
@@ -20,31 +22,29 @@ function enableInputPoints() {
     addPointInputRow();
     document.getElementById('point-inputs').style.display = null
     document.getElementById('render-gif-button').style.display = null
-}   
+}
 
 function addPointInputRow() {
     const newRow = document.createElement("div");
     newRow.className = 'graph-display-point'
-    newRow.innerHTML = '<label>Position X</label><input type="number" class="point-pos-x"></input><label>Position Y</label><input type="number" class="point-pos-y"></input>'
+    newRow.innerHTML = '<label>Position X</label><input type="number" class="point-pos-x" placeholder="0"></input><label>Position Y</label><input type="number" class="point-pos-y" placeholder="0"></input>'
     document
         .getElementById('point-inputs')
         .append(newRow)
 }
 
-function drawRedSquare(ctx) {
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(10, 10, 75, 50); //draw a little red box
-}
-
-function drawBlueCircle(ctx) {
-    ctx.fillStyle = "rgb(0,200,0)";
+function drawPoint(ctx, position) {
+    ctx.drawImage(baseImage, 0, 0);
+    ctx.fillStyle = "rgb(0,0,200,0.5)";
     ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    ctx.fillStyle = "blue";
+    ctx.arc(position[0], position[1], 10, 0, 2 * Math.PI);
     ctx.fill();
 }
 
 function render(canvas, ctx) {
+    const pointsToRender = getPoints()
+    console.log('pointsToRender', pointsToRender)
+
     var $renderimg = document.getElementById('render')
     $renderimg.width = canvas.width
     $renderimg.height = canvas.height
@@ -56,13 +56,12 @@ function render(canvas, ctx) {
         background: '#ffffff',
     });
 
-    gif.addFrame(ctx, { copy: true });
+    gif.addFrame(baseImage)
 
-    drawRedSquare(ctx);
-    gif.addFrame(ctx, { copy: true });
-
-    drawBlueCircle(ctx);
-    gif.addFrame(ctx, { copy: true, delay: 200 });
+    pointsToRender.forEach(point => {
+        drawPoint(ctx, point);
+        gif.addFrame(ctx, { copy: true, delay: 200 });
+    });
 
     gif.on("finished", function (blob) {
         document.getElementById('canvas-container').style.display = 'none'
@@ -77,6 +76,24 @@ function render(canvas, ctx) {
 
 function unhideRender($imageRenderContainer) {
     $imageRenderContainer.style.display = null
+}
+
+function getPoints() {
+    return Array.prototype.slice
+        .call(document.getElementsByClassName('graph-display-point'))
+        .map(getPoint)
+        .filter(x => !!x)
+}
+
+function getPoint($pointRow) {
+    try {
+        return [
+            +($pointRow.getElementsByClassName('point-pos-x')[0].value),
+            +($pointRow.getElementsByClassName('point-pos-y')[0].value),
+        ];
+    } catch (e) {
+        return []
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
